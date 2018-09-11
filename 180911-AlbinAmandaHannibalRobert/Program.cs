352db3e7db1c9e20d6d2dcde142cb993e537ae07
amanda eriksson <stollan97@hotmail.com>
2018-09-11 13:47:29 +0200

    lagt till djur

--- 180911-AlbinAmandaHannibalRobert/Program.cs
+++ 180911-AlbinAmandaHannibalRobert/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace _180911_AlbinAmandaHannibalRobert {
     class Program {
         static void Main(string[] args) {
+            Amanda.Animal();
         }
     }
 }
